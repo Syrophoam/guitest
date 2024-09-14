84353654ec294dcb32130be54f66ba653c574f30
Syrophoam <jamesfullerton2006@gmail.com>
2024-09-14 16:24:41 +1200

    Initial Commit

---
+++ guitest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  guitest
+//
+//  Created by syro Fullerton on 14/09/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
